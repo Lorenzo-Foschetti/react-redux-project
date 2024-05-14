4ba806c2a5c130550457176eefbcb54c64d3be1a
Lorenzo-Foschetti <lorenzo.foschetti00@gmail.com>
2024-05-14 13:31:51 +0200

    try to fix better style of player

--- src/components/MyPlayer.jsx
+++ src/components/MyPlayer.jsx
@@ -11,46 +11,50 @@ const MyPlayer = () => {
 
   return (
     <Container fluid className=" fixed-bottom bg-container pt-1">
-      <div className="row h-100">
-        <div lg={10} className="offset-lg-2 ">
-          <div className="row h-100 flex-column justify-content-center align-items-center">
-            <div className="col-6 col-md-4 d-flex align-items-center justify-content-end">
-              {selectedSong && (
-                <>
-                  <img src={selectedSong.artist.picture_small} alt="" />
-                  <div className="ms-3 text-secondary">
-                    <p className="m-0"> {selectedSong.title}</p>
-                    <p className="m-0"> {selectedSong.artist.name}</p>{" "}
-                  </div>
-                </>
-              )}
-            </div>
-
-            <div className="col-6 col-md-4 playerControls">
-              <div className="d-flex">
-                <NavLink href="#1">
-                  <img src={shuffle} alt="shuffle" />
-                </NavLink>
-                <NavLink href="#1">
-                  <img src={prev} alt="prev" />
-                </NavLink>
-                <NavLink href="#1">
-                  <img src={play} alt="play" />
-                </NavLink>
-                <NavLink href="#1">
-                  <img src={next} alt="next" />
-                </NavLink>
-                <NavLink href="#1">
-                  <img src={repeat} alt="repeat" />
-                </NavLink>
+      <Col>
+        <div className="row h-100">
+          <div lg={10} className="offset-lg-2 ">
+            <div className="row h-100 flex-column justify-content-center align-items-center">
+              <div className="col-6 col-md-4 d-flex align-items-center justify-content-end">
+                {selectedSong && (
+                  <>
+                    <div className="d-flex justify-content-between gx-3 ">
+                      <img src={selectedSong.artist.picture_small} alt="" />
+                      <div className=" text-secondary">
+                        <p className="m-0"> {selectedSong.title}</p>
+                        <p className="m-0"> {selectedSong.artist.name}</p>{" "}
+                      </div>
+                    </div>
+                  </>
+                )}
               </div>
-              <div className="progress mt-3">
-                <div role="progressbar"></div>
+
+              <div className="col-6 col-md-4 playerControls">
+                <div className="d-flex">
+                  <NavLink href="#1">
+                    <img src={shuffle} alt="shuffle" />
+                  </NavLink>
+                  <NavLink href="#1">
+                    <img src={prev} alt="prev" />
+                  </NavLink>
+                  <NavLink href="#1">
+                    <img src={play} alt="play" />
+                  </NavLink>
+                  <NavLink href="#1">
+                    <img src={next} alt="next" />
+                  </NavLink>
+                  <NavLink href="#1">
+                    <img src={repeat} alt="repeat" />
+                  </NavLink>
+                </div>
+                <div className="progress mt-3">
+                  <div role="progressbar"></div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </Col>
     </Container>
   );
 };
